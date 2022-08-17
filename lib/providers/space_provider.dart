b9f944c972ce88c6cbfae14a43720ebf9f60b753
PrinceRidho <ridho0weedy@gmail.com>
2022-08-17 22:55:23 +0700

    modified:   lib/providers/space_provider.dart

--- lib/providers/space_provider.dart
+++ lib/providers/space_provider.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 
 class SpaceProvider extends ChangeNotifier {
   getRecommendedSpaces() async {
-    var result =
-        await http.get('https://bwa-cozy.herokuapp.com/recommended-spaces');
+    var result = await http
+        .get(Uri.http('https://bwa-cozy.herokuapp.com/recommended-spaces', ''));
 
     print(result.statusCode);
     print(result.body);
